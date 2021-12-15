f0414c3ea83f404ca55fd6cad50dd0280cf277d3
Tüze Kuyucu <tuze.kuyucu@disruptive-technologies.com>
2021-12-15 21:29:32 +0100

    Bugfix day 13

--- day13.py
+++ day13.py
@@ -2,16 +2,21 @@ with open('day13.txt') as f:
     lines = f.readlines()
 
 def fold_on_y(matrix, coord):
-  for y in range(coord):
+  for x in range(len(matrix[0])):
+      assert(matrix[coord][x] == 0)
+  start_y= coord - (len(matrix) - coord - 1)
+  for y in range(start_y, coord):
     for x in range(len(matrix[0])):
-      matrix[y][x] |= matrix[len(matrix) - y - 1][x]
+      matrix[y][x] |= matrix[len(matrix) - 1 - (y - start_y)][x]
   return matrix[0:coord]
 
 def fold_on_x(matrix, coord):
   new_matrix = list()
+  start_x = coord - (len(matrix[0]) - coord - 1)
   for y in range(len(matrix)):
-    for x in range(coord):
-      matrix[y][x] |= matrix[y][len(matrix[y]) - 1 - x]
+    for x in range(start_x, coord):
+      assert(matrix[y][coord] == 0)
+      matrix[y][x] |= matrix[y][len(matrix[y]) - 1 - (x - start_x)]
     new_matrix.append(matrix[y][0:coord])
   return new_matrix
 
@@ -49,16 +54,15 @@ for line in matrix:
     no_dots += dot
 
 print(f"part1: {no_dots}")
-print(folds)
 
 for fold in folds:
   if fold[0] == "y":
-    matrix = fold_on_y(matrix, fold[1])
+    matrix = fold_on_y(matrix, fold[1]).copy()
   elif fold[0] == "x":
-    matrix = fold_on_x(matrix, fold[1])
+    matrix = fold_on_x(matrix, fold[1]).copy()
   else:
     assert(False)
-
+print("part2: ")
 for line in matrix:
   print("".join(["#" if entry == 1 else "." for entry in line]))
 
